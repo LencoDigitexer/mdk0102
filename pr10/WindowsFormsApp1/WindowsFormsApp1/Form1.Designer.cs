5c312aa82c72f32b30f8f1c88ab8443aaebfbd63
Илья Евсеенко <addminuser258@gmail.com>
2022-02-02 02:49:25 +0300

    код взлета ракеты

--- pr10/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ pr10/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,19 +29,33 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(327, 732);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ЗАПУСК";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // timer1
             // 
-            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(809, 819);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -52,6 +66,7 @@
 
         #endregion
 
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
     }
 }
--- pr10/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ pr10/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,14 @@ namespace WindowsFormsApp1
         private int x1, y1, x2, y2, r;
         private double a;
         private Pen pen = new Pen(Color.DarkRed, 2);
+        int y = 530;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +31,48 @@ namespace WindowsFormsApp1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.DrawLine(pen, x1, y1, x2, y2); //рисуем секундную стрелку  
+            Graphics g1 = e.Graphics;
+            Graphics g2 = e.Graphics;
+            Graphics g3 = e.Graphics;
+            g.Clear(Color.Blue);
+            SolidBrush peg = new SolidBrush(Color.White);
+            SolidBrush peg1 = new SolidBrush(Color.Yellow);
+            SolidBrush peg2 = new SolidBrush(Color.Yellow);
+            SolidBrush peg3 = new SolidBrush(Color.Blue);
+            GraphicsPath gp = new GraphicsPath(FillMode.Winding);
+            GraphicsPath gp1 = new GraphicsPath(FillMode.Winding);
+            GraphicsPath gp2 = new GraphicsPath(FillMode.Winding);
+            GraphicsPath gp3 = new GraphicsPath(FillMode.Winding);
+            gp.AddPolygon(
+                new Point[] { 
+                    new Point(450, y), 
+                    new Point(465, y + 100), 
+                    new Point(485, y + 100), 
+                    new Point(500, y), 
+                    new Point(475, y - 20), 
+                    new Point(450, y) 
+                });
+            gp1.AddPolygon(
+                new Point[] { 
+                    new Point(465, y + 100), 
+                    new Point(445, y + 120), 
+                    new Point(445, y + 90), 
+                    new Point(460, y + 70), 
+                    new Point(465, y + 100) 
+                });
+            gp2.AddPolygon(
+                new Point[] { 
+                    new Point(485, y + 100), 
+                    new Point(505, y + 120), 
+                    new Point(505, y + 90), 
+                    new Point(490, y + 70), 
+                    new Point(485, y + 100) 
+                });
+            gp3.AddEllipse(465, y + 5, 20, 20);
+            g.FillPath(peg, gp);
+            g1.FillPath(peg1, gp1);
+            g2.FillPath(peg2, gp2);
+            g3.FillPath(peg3, gp3);
         }
         private void Form1_Load(object sender, EventArgs e)
         {   //определяем цент экрана             
@@ -34,14 +84,14 @@ namespace WindowsFormsApp1
             x2 = x1 + (int)(r * Math.Cos(a));
             y2 = y1 - (int)(r * Math.Sin(a));
         }
+
+        int H = 0;
         private void timer1_Tick(object sender, EventArgs e)
         {
-            a -= 0.1;//уменьшаем угол на 0,1 радиану 
-                     //определяем конец часовой стрелки с учетом центра экрана       
-            x2 = x1 + (int)(r * Math.Cos(a));
-            y2 = y1 - (int)(r * Math.Sin(a));
-            Invalidate(); //вынудительный вызов перерисовки (Paint) 
-
+            y--;
+            H++;
+            Invalidate();
+            //textBox1.Text = H.ToString();
         }
     }
 
