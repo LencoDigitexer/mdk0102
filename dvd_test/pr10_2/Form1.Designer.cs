a95946dd5a85dc3a1c4e5f30d6c46c36086724f2
Илья Евсеенко <addminuser258@gmail.com>
2022-02-04 02:16:18 +0300

    dvd logo test

---
+++ dvd_test/pr10_2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace pr10_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(676, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ dvd_test/pr10_2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr10_2
+{
+    public partial class Form1 : Form
+    {   //описываем переменные достыпные в любом обработчике событий класса Form1
+        //
+        private Point PreviousPoint, point; //Точка до перемещения курсора мыши и текущая точка
+        private Graphics g;
+        private int x1, y1, x2, y2, r;
+        private Point center;
+        private float diameter = 30f;
+        private double a;
+        int x, y;
+        int xspeed = 11, yspeed = 11;
+        
+        private Pen pen = new Pen(Color.DarkRed, 2);
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawLine(pen, 30+x, 30 + y, 60 + x, 30 + y);
+            g.DrawLine(pen, 60+x, 30 + y, 70 + x, 40 + y);
+            g.DrawLine(pen, 70+x, 40 + y, 70 + x, 50 + y);
+            g.DrawLine(pen, 70+x, 50 + y, 60 + x, 60 + y);
+            g.DrawLine(pen, 60+x, 60 + y, 30 + x, 60 + y);
+            g.DrawLine(pen, 30+x, 60 + y, 20 + x, 50 + y);
+            g.DrawLine(pen, 20+x, 50 + y, 20 + x, 40 + y);
+            g.DrawLine(pen, 20+x, 40 + y, 30 + x, 30 + y);
+
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = x.ToString() + " " + y.ToString();
+
+            x = x + xspeed;
+            y = y + yspeed;
+            Invalidate();
+
+            if (x + 80 > this.Width || x == 0)
+            {
+                xspeed = xspeed * -1;
+            }
+
+            if(y + 100 > this.Height || y == 0) { 
+                yspeed = yspeed * -1;
+                
+            }
+            
+
+            /*
+            if (x==y)
+            {
+                x++;
+                y++;
+                Invalidate();
+
+            }
+
+            if ((x==400))
+            {
+                x++;
+                y--;
+                Invalidate();
+            }
+            if ((x >y))
+            {
+                x++;
+                y--;
+                Invalidate();
+            }
+
+            if(x == 720)
+            {
+                x--;
+                y--;
+                Invalidate();
+            }
+            if(y<(x-600))
+            {
+                x--;
+                y--;
+                Invalidate();
+            }
+            if(y<0)
+            {
+                x--;
+                y++;
+                Invalidate();
+            }
+            
+            if ((x+y) > 600 && (x>y))
+            {
+                x--;
+                y--;
+                Invalidate();
+            }
+            if (x>800 && y)
+            {
+                x--;
+                y++;
+                Invalidate();
+            }*/
+        }
+    }
+
+}
